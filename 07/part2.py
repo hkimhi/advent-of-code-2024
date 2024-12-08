6cd61fe226b43efee55f83c4478739d19352da62
Hod Kimhi <hodkimhi@gmail.com>
2024-12-08 03:27:59 -0800

    more optimization on day 7 part 1, but could still improve with a backwards solve

--- 07/part2.py
+++ 07/part2.py
@@ -1,4 +1,4 @@
-import itertools
+import time
 
 equations = []
 
@@ -6,29 +6,29 @@ with open("input.txt") as infile:
     equations = [line.strip() for line in infile.readlines()]
 
 result = 0
-OPERATIONS = ['*', '+', 'concat']
 
+def recursive_solver(goal, curr, digits):
+    if len(digits) <= 0:
+        return curr == goal
+
+    op1 = recursive_solver(goal, curr + digits[0], digits[1:])
+    op2 = recursive_solver(goal, curr * digits[0], digits[1:])
+    op3 = recursive_solver(goal, int(f"{curr}{digits[0]}"), digits[1:])
+
+    return op1 or op2 or op3
+
+start_time = time.time()
 for equation in equations:
     goal, rest = equation.split(': ')
     goal = int(goal)
+
     nums = [int(_) for _ in rest.split(' ')]
-    num_spaces = rest.count(' ')
-
-    for ops in itertools.product(OPERATIONS, repeat=num_spaces):
-        curr_val = nums[0]
-        for j, op in enumerate(ops):
-            if op == '*':
-                curr_val *= nums[j + 1]
-            elif op == '+':
-                curr_val += nums[j + 1]
-            elif op == 'concat':
-                curr_val = int(str(curr_val) + str(nums[j + 1]))
-
-        if curr_val == goal:
-            result += goal
-            break
+    if (recursive_solver(goal, nums[0], nums[1:])):
+        result += goal
 
 print(f"total calibration result: {result}")  # 61561126043536
-# slightly better solution, maybe? should probably run a profiler, still took a while
+print(f"took {time.time() - start_time} seconds")
+# still slightly better with a "forward" solve using recursion, but also taking a while (3.6s)
+
 # better move would probably to work backwards since we get only increasing operators, so can see
 # if the decrease is legal (i.e. is the next number a factor? if not can't multiply, skip that path)
