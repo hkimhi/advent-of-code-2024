ce76f5698d223fce6d383f880064154b6759c0f5
Hod Kimhi <hodkimhi@gmail.com>
2024-12-08 03:16:59 -0800

    slight optimization to day 7 part 2?

--- 07/part2.py
+++ 07/part2.py
@@ -1,4 +1,4 @@
-from enum import Enum
+import itertools
 
 equations = []
 
@@ -6,36 +6,29 @@ with open("input.txt") as infile:
     equations = [line.strip() for line in infile.readlines()]
 
 result = 0
-
-class Operations(Enum):
-    MULTIPLY = 0
-    ADD = 1
-    CONCATENATE = 2
+OPERATIONS = ['*', '+', 'concat']
 
 for equation in equations:
     goal, rest = equation.split(': ')
     goal = int(goal)
     nums = [int(_) for _ in rest.split(' ')]
     num_spaces = rest.count(' ')
-    num_permutations = 3**num_spaces
 
-    for i in range(num_permutations):
-        my_str = ""
+    for ops in itertools.product(OPERATIONS, repeat=num_spaces):
         curr_val = nums[0]
-        for j in range(num_spaces):
-            operation = (i // (3 ** j)) % 3
-            op = Operations(operation)
-            
-            if op == Operations.MULTIPLY:
-                curr_val *= nums[j+1]
-            elif op == Operations.ADD:
-                curr_val += nums[j+1]
-            elif op == Operations.CONCATENATE:
-                curr_val = int(str(curr_val) + str(nums[j+1]))
+        for j, op in enumerate(ops):
+            if op == '*':
+                curr_val *= nums[j + 1]
+            elif op == '+':
+                curr_val += nums[j + 1]
+            elif op == 'concat':
+                curr_val = int(str(curr_val) + str(nums[j + 1]))
 
         if curr_val == goal:
             result += goal
             break
 
 print(f"total calibration result: {result}")  # 61561126043536
-# LMAO naive solution takes a while (obviously, 3**n is kinda large compared to 2**n)
+# slightly better solution, maybe? should probably run a profiler, still took a while
+# better move would probably to work backwards since we get only increasing operators, so can see
+# if the decrease is legal (i.e. is the next number a factor? if not can't multiply, skip that path)
