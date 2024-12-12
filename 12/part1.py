bdd9cc4d235d982b3009cba70961a8d713e07e87
Hod Kimhi <hodkimhi@gmail.com>
2024-12-12 02:29:30 -0800

    day 12 part 1

--- 12/part1.py
+++ 12/part1.py
@@ -2,7 +2,7 @@ import time
 
 start_time = time.time()
 
-with open("example2.txt") as infile:
+with open("input.txt") as infile:
     garden = [list(line.strip()) for line in infile.readlines()]
 
 DIRECTIONS = [
@@ -12,49 +12,43 @@ DIRECTIONS = [
     (0, -1)
 ]
 
-areas = {}
-perimeters = {}
+def dfs(x, y, visited, plant_type):
+    stack = [(x, y)]
+    area = 0
+    perimeter = 0
 
-# algorithm?
-# start on first plot
-# try in each direction, recurse while same plant
-# if new plant, start new bfs?
-def dfs(x, y, visited):
-    if (x, y) in visited:
-        return
+    while stack:
+        cx, cy = stack.pop()
+        if (cx, cy) in visited:
+            continue
+        visited.add((cx, cy))
+        area += 1
+        local_perimeter = 4
 
-    visited.add((x, y))
+        for dx, dy in DIRECTIONS:
+            nx, ny = cx + dx, cy + dy
+            if 0 <= nx < len(garden) and 0 <= ny < len(garden[0]):
+                if garden[nx][ny] == plant_type:
+                    stack.append((nx, ny))
+                    local_perimeter -= 1
 
-    curr_letter = garden[x][y]
-    if curr_letter in areas:
-        areas[curr_letter] += 1
-    else:
-        areas[curr_letter] = 1
-    num_adjacent = 0
+        perimeter += local_perimeter
 
-    for (dx, dy) in DIRECTIONS:
-        if 0 <= x + dx < len(garden) and 0 <= y + dy < len(garden[0]):
-            letter = garden[x + dx][y + dy]
-            if curr_letter == letter:
-                num_adjacent += 1
+    return area, perimeter
 
-    curr_perimeter = 4 - num_adjacent
-    if curr_letter not in perimeters:
-        perimeters[curr_letter] = curr_perimeter
-    else:
-        perimeters[curr_letter] += curr_perimeter
+def calculate_cost():
+    visited = set()
+    total_cost = 0
 
-    for (dx, dy) in DIRECTIONS:
-        if 0 <= x + dx < len(garden) and 0 <= y + dy < len(garden[0]):
-            dfs(x + dx, y + dy, visited)
+    for x in range(len(garden)):
+        for y in range(len(garden[0])):
+            if (x, y) not in visited:
+                plant_type = garden[x][y]
+                area, perimeter = dfs(x, y, visited, plant_type)
+                cost = area * perimeter
+                total_cost += cost
 
-dfs(0, 0, set())
+    return total_cost
 
-price = 0
-for letter in areas:
-    price += areas[letter] * perimeters[letter]
-
-breakpoint()
-
-print(f"total price: {price}")
+print(f"total price: {calculate_cost()}")  # 1477762
 print(f"took {time.time() - start_time}s")
