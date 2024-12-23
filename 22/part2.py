4373632493a2fc4ee96670337a13bc977931dcdb
Hod Kimhi <hodkimhi@gmail.com>
2024-12-23 00:56:08 -0600

    add documentation

--- 22/part2.py
+++ 22/part2.py
@@ -19,20 +19,25 @@ def gen_next(num):
 
     return num
 
+# total_bananas stores key: sequence -> val: total number of bananas gotten by telling all monkeys that sequence
 total_bananas = defaultdict(int)
+# breakpoint()
 for num in initial_numbers:
-    sequences = {}
-    sequence = (0, 0, 0, 0)
+    sequences = {}  # for the current monkey
+    sequence = (0, 0, 0, 0)  # initial sequence is no price changes
     for i in range(2000):
         previous_bananas = num % 10
         num = gen_next(num)
         current_bananas = num % 10
         sequence = (*sequence[1:], current_bananas - previous_bananas)
+
+        # if we have a valid sequence (at least 3 price changes), add it to the dict
+        # sequences stores key: sequence -> val: num bananas
         if i >= 3 and sequence not in sequences:
             sequences[sequence] = current_bananas
-    for seq in sequences:
-        total_bananas[seq] += sequences[seq]
+            total_bananas[sequence] += current_bananas  # total number of bananas for this sequence across all monkeys
 
+# get the highest number of bananas possible (and the sequence that arrived there if we want it)
 best = sorted([(value, key) for key, value in total_bananas.items()])[-1]
 
 print(best)    # 1986
