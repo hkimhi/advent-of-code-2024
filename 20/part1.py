99437524197c17fe90fdcbcfe9664d044c45b71d
Hod Kimhi <hodkimhi@gmail.com>
2024-12-29 16:44:01 -0800

    updated and fixed day 20 part 1

--- 20/part1.py
+++ 20/part1.py
@@ -42,9 +42,11 @@ def a_star(map, width, height):
     while pq:
         cost, pos, moves, path = heappop(pq)
         if pos == end:
-            path_and_cost = [(start, 0)]  # inital start position
+            path_and_cost = {
+                start: 0
+            }
             for i in range(len(path)):
-                path_and_cost += [(path[i], i + 1)]  # tile and cost at that tile
+                path_and_cost[path[i]] = i + 1  # tile and cost at that tile
             return moves, path_and_cost
 
         for d in DIRECTIONS:
@@ -60,7 +62,6 @@ def a_star(map, width, height):
 def cheating_a_star(map, width, height, path, base_cost):
     DIRECTIONS = [1, width, -1, -width]
 
-    start = map.index("S")
     end = map.index("E")
     map = map[:end] + '.' + map[end + 1:]  # replace "E" with '.'
     xe, ye = end // width, end % width
@@ -70,38 +71,18 @@ def cheating_a_star(map, width, height, path, base_cost):
         return abs(x - xe) + abs(y - ye)
 
     num_cheats = 0
-    for tile, tile_cost in path:
-        costs = {}
-        costs[start] = 0
-        pq = []
-
+    for idx, (tile, tile_cost) in enumerate(path.items()):
         # attempt a cheat
         for d_cheat in DIRECTIONS:
             ntile = tile + d_cheat
             nntile = tile + 2 * d_cheat
-            in_bounds = 0 <= ntile, nntile < width * height
-            if all(in_bounds) and map[ntile] == '#' and map[nntile] == '.':
-                # cheat possible
-                heappush(pq, (0, nntile, tile_cost + 2))
-
-                # run a_star from new location (after cheating)
-                while pq:
-                    cost, pos, moves = heappop(pq)
-                    if pos == end:
-                        if base_cost - moves > 100:
-                        # if moves < base_cost:
-                            # print(f"good cheat saving {base_cost - moves}ps from {tile} to {nntile} in dir {d_cheat}")
-                            # print(f"good cheat saving {base_cost - moves}ps from ({tile // width}, {tile % width}) to ({nntile // width}, {nntile % width}) in dir {d_cheat}")
-                            num_cheats += 1
-                        break
-
-                    for d in DIRECTIONS:
-                        npos = pos + d
-                        if 0 <= npos < (width * height) and map[npos] != '#':
-                            ncost = cost + 1
-                            if npos not in costs or ncost < costs[npos]:
-                                costs[npos] = ncost
-                                heappush(pq, (ncost + h(npos), npos, moves + 1))
+            if ntile not in path and nntile in path:
+                # cheat exists
+                saved_moves = path[nntile] - (path[tile] + 2)  # must be positive to be a _good_ cheat
+                if saved_moves >= 100:
+                    # cheat was TOWARDS the end, not backwards
+                    # print(f"cheat from {tile} to {nntile} saves {saved_moves}ps")
+                    num_cheats += 1
 
     return num_cheats
 
@@ -110,8 +91,8 @@ if __name__ == "__main__":
 
     map, (w, h) = load_data("input.txt")
     optimal_path_length, path = a_star(map, w, h)
-    num_good_cheats = cheating_a_star(map, w, h, path, optimal_path_length)
     # print(f"optimal path length: {optimal_path_length}")
-    print(f"number of cheats: {num_good_cheats}")  # example.txt --> 44
+    num_good_cheats = cheating_a_star(map, w, h, path, optimal_path_length)
+    print(f"number of cheats: {num_good_cheats}")  # 1441
 
     print(f"took {time.time() - start_time}s")
